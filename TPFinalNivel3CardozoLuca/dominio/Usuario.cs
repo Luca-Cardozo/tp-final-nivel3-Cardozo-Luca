c6008f568e238a3d2bb0fe6b91c5f249b57b2dab
Luca Cardozo <lucacardozo99@gmail.com>
2026-07-07 16:34:33 -0300

    Agregago de clase de dominio Usuario y de UsuarioNegocio

---
+++ TPFinalNivel3CardozoLuca/dominio/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Imagen { get; set; }
+        public bool Admin { get; set; }
+        public List<Articulo> Favoritos { get; set; }
+    }
+}
