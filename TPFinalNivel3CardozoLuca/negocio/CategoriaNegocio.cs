0289c7e0d7d38f695625ffea6444786a534ba6a7
Luca Cardozo <lucacardozo99@gmail.com>
2026-07-07 16:50:57 -0300

    Agregado de métodos para listar, agregar, modificar y eliminar Marcas y Categorias. Se agregan validaciones también para evitar el duplicado y la eliminación de marcas y categorías con artículos asociados

--- TPFinalNivel3CardozoLuca/negocio/CategoriaNegocio.cs
+++ TPFinalNivel3CardozoLuca/negocio/CategoriaNegocio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Acceso_Datos;
+using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,150 @@ namespace negocio
 {
     public class CategoriaNegocio
     {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion From CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void agregar(string categoriaNueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (existeDescripcion(categoriaNueva))
+                    throw new Exception("Ya existe una categoría con esa descripción.");
+
+                datos.setearConsulta("INSERT INTO CATEGORIAS (Descripcion) VALUES (@Descripcion)");
+                datos.setearParametro("@Descripcion", categoriaNueva);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Categoria categoriaModificada)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (existeDescripcion(categoriaModificada.Descripcion, categoriaModificada.Id))
+                    throw new Exception("Ya existe otra categoría con esa descripción.");
+
+                datos.setearConsulta("UPDATE CATEGORIAS SET Descripcion = @Descripcion WHERE Id = @Id");
+                datos.setearParametro("@Id", categoriaModificada.Id);
+                datos.setearParametro("@Descripcion", categoriaModificada.Descripcion);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int idCategoria)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (tieneArticulosAsociados(idCategoria))
+                    throw new Exception("No se puede eliminar la categoría porque tiene artículos asociados.");
+
+                datos.setearConsulta("DELETE FROM CATEGORIAS WHERE Id = @Id");
+                datos.setearParametro("@Id", idCategoria);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public bool tieneArticulosAsociados(int idCategoria)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id FROM Articulos WHERE IdCategoria = @Id");
+                datos.setearParametro("@Id", idCategoria);
+                datos.ejecutarLectura();
+                if (datos.Lector.Read())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public bool existeDescripcion(string descripcion, int idExcluir = 0)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id FROM CATEGORIAS WHERE Descripcion = @Descripcion AND Id <> @IdExcluir");
+                datos.setearParametro("@Descripcion", descripcion);
+                datos.setearParametro("@IdExcluir", idExcluir);
+                datos.ejecutarLectura();
+                return datos.Lector.Read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
 
     }
 }
--- TPFinalNivel3CardozoLuca/negocio/MarcaNegocio.cs
+++ TPFinalNivel3CardozoLuca/negocio/MarcaNegocio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Acceso_Datos;
+using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,150 @@ namespace negocio
 {
     public class MarcaNegocio
     {
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Select Id, Descripcion From MARCAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void agregar(string marcaNueva)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (existeDescripcion(marcaNueva))
+                    throw new Exception("Ya existe una marca con esa descripción.");
+
+                datos.setearConsulta("INSERT INTO MARCAS (Descripcion) VALUES (@Descripcion)");
+                datos.setearParametro("@Descripcion", marcaNueva);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Marca marcaModificada)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (existeDescripcion(marcaModificada.Descripcion, marcaModificada.Id))
+                    throw new Exception("Ya existe otra marca con esa descripción.");
+
+                datos.setearConsulta("UPDATE MARCAS SET Descripcion = @Descripcion WHERE Id = @Id");
+                datos.setearParametro("@Id", marcaModificada.Id);
+                datos.setearParametro("@Descripcion", marcaModificada.Descripcion);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int idMarca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                if (tieneArticulosAsociados(idMarca))
+                    throw new Exception("No se puede eliminar la marca porque tiene artículos asociados.");
+
+                datos.setearConsulta("DELETE FROM MARCAS WHERE Id = @Id");
+                datos.setearParametro("@Id", idMarca);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public bool tieneArticulosAsociados(int idMarca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id FROM Articulos WHERE IdMarca = @Id");
+                datos.setearParametro("@Id", idMarca);
+                datos.ejecutarLectura();
+                if (datos.Lector.Read())
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public bool existeDescripcion(string descripcion, int idExcluir = 0)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id FROM MARCAS WHERE Descripcion = @Descripcion AND Id <> @IdExcluir");
+                datos.setearParametro("@Descripcion", descripcion);
+                datos.setearParametro("@IdExcluir", idExcluir);
+                datos.ejecutarLectura();
+                return datos.Lector.Read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
 
     }
 }
