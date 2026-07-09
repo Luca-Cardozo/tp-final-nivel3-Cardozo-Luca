31cc707d15dca71de7801f065b9fc3b0a4c6fa5c
Luca Cardozo <lucacardozo99@gmail.com>
2026-07-08 22:05:37 -0300

    Agregado de clase de Seguridad y de EmailService

---
+++ TPFinalNivel3CardozoLuca/app-web/Seguridad.cs
@@ -0,0 +1,28 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace app_web
+{
+    public static class Seguridad
+    {
+        public static bool sesionActiva(object usuario)
+        {
+            return usuario != null;
+        }
+
+        public static Usuario usuarioActual(HttpSessionState session)
+        {
+            return session["usuario"] as Usuario;
+        }
+
+        public static bool esAdmin(HttpSessionState session)
+        {
+            Usuario usuario = usuarioActual(session);
+            return usuario != null && usuario.Admin;
+        }
+    }
+}
---
+++ TPFinalNivel3CardozoLuca/negocio/EmailService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace negocio
+{
+    public class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        // Cuenta emisora
+        private string remitente = ConfigurationManager.AppSettings["Email"];
+        private string password = ConfigurationManager.AppSettings["PasswordEmail"];
+
+        public EmailService()
+        {
+            server = new SmtpClient();
+
+            server.Host = "smtp.gmail.com";
+            server.Port = 587;
+            server.EnableSsl = true;
+            server.DeliveryMethod = SmtpDeliveryMethod.Network;
+            server.UseDefaultCredentials = false;
+
+            server.Credentials = new NetworkCredential(remitente, password);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        public void armarCorreo(string emailDestino, string asunto, string cuerpo)
+        {
+            email = new MailMessage();
+
+            email.From = new MailAddress(remitente, "Catálogo Artículos Tecnológicos");
+            email.To.Add(emailDestino);
+
+            email.Subject = asunto;
+            email.Body = cuerpo;
+            email.IsBodyHtml = true;
+        }
+
+        public void enviarEmail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (SmtpException ex)
+            {
+                throw new Exception(
+                    $"SMTP ERROR\n" +
+                    $"StatusCode: {ex.StatusCode}\n" +
+                    $"Message: {ex.Message}\n" +
+                    $"Inner: {ex.InnerException}"
+                );
+            }
+        }
+    }
+}
